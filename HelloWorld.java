9e7201f32bb48a862ffcb7d0284c65079492ded5
Aidan Moxham <114097171+s1335933@users.noreply.github.com>
2023-09-09 21:29:28 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+	
+	public static void main(String[] args)
+	{
+		//CS176L
+		
+		//Aidan Moxham 
+		
+		//helloWorld
+		System.out.println("Hello World");
+		
+		
+	}
+
+}
