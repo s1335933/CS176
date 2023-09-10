f0cba129bdfc67676d46dd37f5133871f65358f0
Aidan Moxham <114097171+s1335933@users.noreply.github.com>
2023-09-09 21:46:18 -0400

    my first java program version 2

---
+++ HelloWorld2.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld2 {
+	
+	public static void main(String[] args)
+	{
+		//CS176L
+		
+		//Aidan Moxham 
+		
+		//helloWorld
+		System.out.println("Hello World my name is Aidan Moxham");
+		
+		
+	}
+
+}
