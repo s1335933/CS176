631ebf16a67e9dbe5f3c5b518bc6ac7bc2c7c7a3
Aidan Moxham <114097171+s1335933@users.noreply.github.com>
2023-10-08 14:24:16 -0400

    Add files via upload

---
+++ ArrayAssignment1.java
@@ -0,0 +1,12 @@
+public class ArrayAssignment1 {
+    public static void main(String[] args) {
+        
+        double[] values = {25.3, 100, -10, -1.5, 13, 98.6, 17, 123.145, 125.6, 123.146};
+
+        for (double value : values) {
+            System.out.print(value + " ");
+        }
+
+        System.out.println();
+    }
+}
